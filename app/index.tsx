6636736fa4af301e531382f695260ad822933f23
Erwin Doria <Erwin@Erwins-MacBook-Air.local>
2022-02-21 14:40:04 +0800

    WIP

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import { Text, UIManager } from 'react-native';
+import { PushNotificationPermissions, Text, UIManager } from 'react-native';
 import { Provider } from 'react-redux';
 import AppWrapper from './AppWrapper';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { isAndroid } from './hooks/platform';
+import PushNotification from 'react-native-push-notification';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
 // Text.defaultProps = {
 //     ...Text.defaultProps,
@@ -15,9 +17,64 @@ if (isAndroid && UIManager.setLayoutAnimationEnabledExperimental) {
     UIManager.setLayoutAnimationEnabledExperimental(true);
 }
 
+PushNotification.configure({
+    // (optional) Called when Token is generated (iOS and Android)
+    onRegister: function (token) {
+        console.log('TOKEN:', token);
+    },
+
+    // (required) Called when a remote is received or opened, or local notification is opened
+    onNotification: function (notification) {
+        console.log('NOTIFICATION:', notification);
+
+        // process the notification
+
+        // (required) Called when a remote is received or opened, or local notification is opened
+        notification.finish(PushNotificationIOS.FetchResult.NoData);
+    },
+
+    // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+    onAction: function (notification) {
+        console.log('ACTION:', notification.action);
+        console.log('NOTIFICATION:', notification);
+
+        // process the action
+    },
+
+    // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+    onRegistrationError: function (err) {
+        console.error(err.message, err);
+    },
+
+    // IOS ONLY (optional): default: all - Permissions to register.
+    permissions: {
+        alert: true,
+        badge: true,
+        sound: true,
+    },
+
+    // Should the initial notification be popped automatically
+    // default: true
+    popInitialNotification: true,
+
+    /**
+     * (optional) default: true
+     * - Specified if permissions (ios) and token (android and ios) will requested or not,
+     * - if not, you must call PushNotificationsHandler.requestPermissions() later
+     * - if you are not using remote notification or do not have Firebase installed, use this:
+     *     requestPermissions: Platform.OS === 'ios'
+     */
+    requestPermissions: true,
+});
+
+PushNotification.checkPermissions(
+    (permissions: PushNotificationPermissions) => {
+        console.log(permissions);
+    },
+);
+
 const App: React.FC<any> = () => {
     const Loading = <Text>Loading...</Text>;
-
     return (
         <Provider store={store}>
             <PersistGate persistor={persistor} loading={Loading}>
